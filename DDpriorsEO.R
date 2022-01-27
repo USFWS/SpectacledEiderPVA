1c933ad14056140a54c0b94a21fd92b8d68c1643
Catherine Bradley <catherine_bradley@fws.gov>
2022-01-27 11:02:17 -0800

    corrected BP in Btest; added commented results

--- DDpriorsEO.R
+++ DDpriorsEO.R
@@ -30,7 +30,7 @@ Atest = function(N=10000, betaN=0.01){
 
 Btest = function(betaN=0.01, N=12500){
   ilogit <- function(x){exp(x)/(1+exp(x))}
-  BP <- ilogit(1.73) # assumed mean BP was 0.9
+  BP <- ilogit(2.2) # assumed mean BP was 0.9
   mean.phi0 <- 0.325 # phi.0 + (1-phi.0)*omegaJ, from cjs and EE
   mean.logit.phi0 <- log(mean.phi0/(1-mean.phi0))
   mean.phiA <- 0.9 # phi.A + (1-phi.A)*omegaA, from cjs and EE
@@ -52,31 +52,31 @@ Btest = function(betaN=0.01, N=12500){
   return( (max(Re(eigen(A)$values))-1)^2 )
 }
 
-optimize(f=Atest, interval = c(0, 1e9), betaN = 0.001)
+optimize(f=Atest, interval = c(0, 1e7), betaN = 0.0001)
 
-optimize(f=Btest, interval = c(0, 0.001), N=12500)
+optimize(f=Btest, interval = c(0, 0.1), N=12500)
 
 ##relate this to the EE for the ACP
 #high K = 50657 ibb = 50657/2 females = 25328.5
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=25329)$minimum
-
+# [1] 0.008039082
 #best K = 25000 ibb = 12500 females
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=12500)$minimum
-
+# [1] 0.01628975
 #low K = 10743 ibb = 5371.5 females
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=5372)$minimum
-
+# [1] 0.0379043
 
 ##relate this to the EE for the YKD
 #high females = 37.5K
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=37500)$minimum
-
+# [1] 0.005429917
 #best  = 20K females
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=20000)$minimum
-
+# [1] 0.0101811
 #low = 8.5K  females
 optimize(f=Btest, interval = c(0, 0.1), tol=1e-8, N=8500)$minimum
-
+# [1] 0.02395552
 
 
 #now find a distribution that matches the EE
@@ -87,16 +87,45 @@ qmatch.gamma <- function(par=c(3,10000), eeq=c(0.1, 0.7, 2.3), qtarget=c(0.1, 0.
 
 #ACP
 fit <- optim(par=c(3, 10000), fn=qmatch.gamma, method="L-BFGS-B", lower=c(1,1), 
-             eeq=c(0.006937275, 0.01405714, 0.03270927))
+             eeq=c(0.008039082, 0.01628975, 0.0379043))
 fit
+# $par
+# [1]   2.5295 124.3611
+# 
+# $value
+# [1] 430.4592
+# 
+# $counts
+# function gradient 
+# 58       58 
+# 
+# $convergence
+# [1] 0
+# 
+# $message
+# [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
 
 qgamma(c(0.1, 0.5, 0.9), shape=fit$par[1], rate=fit$par[2])
-
+# [1] 0.006610512 0.017731524 0.037474102
 #YKD
 fit <- optim(par=c(3, 10000), fn=qmatch.gamma, method="L-BFGS-B", lower=c(1,1), 
-             eeq=c(0.004685713, 0.008785711, 0.02067226))
+             eeq=c(0.005429917, 0.0101811, 0.02395552))
 fit
-
+# $par
+# [1]   2.623867 203.060233
+# 
+# $value
+# [1] 265.7278
+# 
+# $counts
+# function gradient 
+# 53       53 
+# 
+# $convergence
+# [1] 0
+# 
+# $message
+# [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
 qgamma(c(0.1, 0.5, 0.9), shape=fit$par[1], rate=fit$par[2])
-
+# [1] 0.004317903 0.011322344 0.023611622
 hist(rgamma(10000, fit$par[1], fit$par[2]))
